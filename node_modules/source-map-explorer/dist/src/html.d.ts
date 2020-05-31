5f0455c6f2343dec75d7d2753c9332dd2910d1d0
Joseph Semrai <josephsemrai@gmail.com>
2020-05-31 00:42:13 -0400

    remove gitignore files

--- node_modules/source-map-explorer/dist/src/html.d.ts
+++
@@ -1,5 +0,0 @@
-import { ExploreBundleResult } from './index';
-/**
- * Generate HTML file content for specified files
- */
-export declare function generateHtml(exploreResults: ExploreBundleResult[]): string;
--- node_modules/source-map-explorer/dist/src/output.d.ts
+++
@@ -1,3 +0,0 @@
-import { ExploreBundleResult, ExploreOptions, ExploreResult } from './index';
-export declare function formatOutput(results: ExploreBundleResult[], options: ExploreOptions): string | undefined;
-export declare function saveOutputToFile(result: ExploreResult, options: ExploreOptions): void;
