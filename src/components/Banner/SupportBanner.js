d8fa8a87e18723063646be5ebb3481a7cbb6ccf2
Joseph Semrai <josephsemrai@gmail.com>
2020-06-16 21:31:39 -0400

    Update SupportBanner.js

--- src/components/Banner/SupportBanner.js
+++ src/components/Banner/SupportBanner.js
@@ -9,7 +9,7 @@ const SupportBanner =()=>{
                         <div className="col-md-12 text-center">
                             <h2 className="f_p f_size_40 l_height60 wow fadeInUp" data-wow-delay="0.3s">Free, quality, accessible <br/><span className="f_700">Computer Science</span> courses</h2>
                             <p className="f_size_18 l_height30 wow fadeInUp" data-wow-delay="0.5s">Learn more about Computer Science with our unique courses.</p>
-                            <form className="mailchimp wow fadeInUp" data-wow-delay="0.6s" method="post" action="https://account.codefycs.org/">
+                            <form className="mailchimp wow fadeInUp" data-wow-delay="0.6s" method="post" action="https://formspree.io/mzbjvpye">
                                 <div className="input-group subcribes">
                                     <input type="text" name="EMAIL" className="form-control memail" placeholder="Enter your email..."/>
                                     <button className="btn btn_submit f_size_15 f_500" type="submit">Get Started</button>
